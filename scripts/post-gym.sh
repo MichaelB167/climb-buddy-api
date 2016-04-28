ecbf0e90b4882d3040575f9e7866407ef8640604
MichaelB167 <michael.m.bates@gmail.com>
2016-04-28 14:10:53 -0400

    modify post script

---
+++ scripts/post-gym.sh
@@ -0,0 +1,13 @@
+TOKEN= ''
+
+curl --include --request POST http://localhost:3000/stores/1/gyms \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "gyms": {
+      "name": "Central Rock",
+      "location": "Watertown",
+      "date_visited": "2016-4-20",
+      "gym_id": "1"
+    }
+  }'
