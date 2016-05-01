82d28b0a5746f66f1cce395a1f86e71eaf63f588
MichaelB167 <michael.m.bates@gmail.com>
2016-05-01 18:45:24 -0400

    Working get curl script

--- scripts/get-climbs.sh
+++ scripts/get-climbs.sh
@@ -1,1 +1,4 @@
-curl --include --request GET http://localhost:3000/climbs
+  TOKEN='BAhJIiU1ZWNjODEwNzdlOWY4OTY1MTllY2M4YWY1NWNhYzVjNAY6BkVG--43db99a12158360beab14fc56076fca42ba4bf9e'
+  curl --include --request GET http://localhost:3000/climbs/ \
+    --header "Authorization: Token token=$TOKEN" \
+    --header "Content-Type: application/json" \
