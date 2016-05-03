8fba8dc2da5cd2ceb8b4733e64924d50a8fb9601
MichaelB167 <michael.m.bates@gmail.com>
2016-05-02 20:12:33 -0400

    Working delete curl script

--- scripts/delete-climb.sh
+++ scripts/delete-climb.sh
@@ -1,3 +1,4 @@
-TOKEN=''
-curl --include --request DELETE http://localhost:3000/climbs/1 \
-  --header "Authorization: Token token=$TOKEN" \
+    TOKEN='BAhJIiU5NDU3ODBkMjliMDAxZjM3OWI2ODBhYzVlM2E5OTZlNwY6BkVG--af3044b436eb5361df3669c71a2578fd4a152040'
+    ID='34'
+    curl --include --request DELETE http://localhost:3000/climbs/$ID \
+      --header "Authorization: Token token=$TOKEN" \
